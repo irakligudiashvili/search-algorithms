88cabaf231044c9db2e1732082b4f740684e160d
Irakli Gudiashvili <igudiashvili99@gmail.com>
2026-05-16 21:53:59 +0400

    Create City model

---
+++ Models/City.cs
@@ -0,0 +1,13 @@
+﻿namespace SearchAlgorithms.Models {
+    public class City {
+        public string Name { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+
+        public City(string name, double lat, double lon) {
+            Name = name;
+            Lat = lat;
+            Lon = lon;
+        }
+    }
+}
