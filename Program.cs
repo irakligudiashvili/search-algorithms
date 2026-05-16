da1ca437451ecb654f5b70cb93e03a4e83a478e3
Irakli Gudiashvili <igudiashvili99@gmail.com>
2026-05-16 21:53:48 +0400

    Update default namespace name

--- Program.cs
+++ Program.cs
@@ -1,12 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace search_algorithms
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
+namespace SearchAlgorithms {
+    public class Program {
+        public static async Task Main(string[] args) {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
