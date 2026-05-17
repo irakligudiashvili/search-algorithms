4c42729fdf99e29a4fbaa056d1eff4c5913834e8
Irakli Gudiashvili <igudiashvili99@gmail.com>
2026-05-17 22:54:51 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SearchAlgorithms.Services;
 
 namespace SearchAlgorithms {
     public class Program {
@@ -9,6 +10,8 @@ namespace SearchAlgorithms {
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<BreadthFirstSearch>();
+            builder.Services.AddScoped<IterativeDeepeningBidirectionalDFS>();
 
             await builder.Build().RunAsync();
         }
