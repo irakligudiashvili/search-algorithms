f824bcbeca467595b0a285a2db121861b83b31f8
Irakli Gudiashvili <igudiashvili99@gmail.com>
2026-05-17 12:48:48 +0400

    Add roads

---
+++ Models/Road.cs
@@ -0,0 +1,13 @@
+﻿namespace SearchAlgorithms.Models {
+    public class Road {
+        public string StartCity { get; set; }
+        public string EndCity { get; set; }
+        public double ControlX { get; set; }
+        public double ControlY { get; set; }
+
+        public Road(string startCity, string endCity) {
+            StartCity = startCity;
+            EndCity = endCity;
+        }
+    }
+}
