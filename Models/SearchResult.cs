d6617658cdd4802a774fde6dcf951932e218f27b
Irakli Gudiashvili <igudiashvili99@gmail.com>
2026-05-17 22:54:08 +0400

    Add SearchResult model

---
+++ Models/SearchResult.cs
@@ -0,0 +1,9 @@
+﻿namespace SearchAlgorithms.Models {
+    public class SearchResult {
+        public List<string> Path { get; set; } = new();
+        public List<string> VisitedOrder { get; set; } = new();
+        public TimeSpan ExecutionTime { get; set; }
+        public int NodesExploredCount => VisitedOrder.Count;
+        public double TotalDistance { get; set; }
+    }
+}
